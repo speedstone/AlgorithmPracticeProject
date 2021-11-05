704ad3d3471274f4a6958d53f4b5eb37d6170e03
Lei 郑 <leizheng@LeideMacBook-Pro.local>
2021-11-05 20:56:02 +0800

    add

---
+++ algorithm/src/algorithm/test/Test.java
@@ -0,0 +1,12 @@
+package algorithm.test;
+
+/**
+ * @author: leizheng
+ * @create: 2021/11/5 8:46 下午
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
